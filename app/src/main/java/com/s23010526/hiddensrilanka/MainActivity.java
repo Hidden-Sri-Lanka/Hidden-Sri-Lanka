17fc14a17f82e81d669fb927a4203e6f4a859be2
Asithakanchana1 <asitha.contact@gmail.com>
2025-06-18 15:20:54 +0530

    Splash Screen[auto loading welcome Screen {Delay 5 second} , Fade in Animation Used](Compleated)

--- app/src/main/java/com/s23010526/hiddensrilanka/MainActivity.java
+++ app/src/main/java/com/s23010526/hiddensrilanka/MainActivity.java
@@ -2,6 +2,8 @@ package com.s23010526.hiddensrilanka;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
 
 import androidx.activity.EdgeToEdge;
@@ -12,11 +14,28 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private static final long SPLASH_TIMEOUT = 3000; // 3 seconds Delay between next screen
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        // Use a Handler to delay the transition
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Create an Intent to start the new activity
+                Intent intent = new Intent(MainActivity.this, WelcomeActivity.class);
+                startActivity(intent);
+
+                // Finish this activity so the user can't navigate back to the splash screen otherwise when user is pressing bacjk button thaywiil go to splash screen
+
+                finish();
+            }
+        }, SPLASH_TIMEOUT);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
