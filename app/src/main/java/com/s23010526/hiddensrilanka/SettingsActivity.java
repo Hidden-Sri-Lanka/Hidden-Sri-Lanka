92c08e3ea451e58c25cd9a0dd61b42b90eb1e3c0
Asithakanchana1 <asitha.contact@gmail.com>
2025-06-20 11:26:13 +0530

    Settings Page[Top Toolbar](WorkableState)

--- app/src/main/java/com/s23010526/hiddensrilanka/SettingsActivity.java
+++ app/src/main/java/com/s23010526/hiddensrilanka/SettingsActivity.java
@@ -1,12 +1,17 @@
 package com.s23010526.hiddensrilanka;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 // Import the correct Toolbar from androidx.appcompat
-import androidx.appcompat.widget.Toolbar; // <--- CHANGE THIS
+import androidx.appcompat.widget.Toolbar;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -16,44 +21,91 @@ import com.google.android.material.navigation.NavigationView;
 
 public class SettingsActivity extends AppCompatActivity {
 
-    //Variables for Nav Viwe
     DrawerLayout drawerLayout;
     NavigationView navigationView;
-    Toolbar toolbar; // This will now be androidx.appcompat.widget.Toolbar
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+        // EdgeToEdge is not needed when manually setting padding for system bars.
+        // You can comment it out or remove it if it causes issues with the toolbar color.
+        // EdgeToEdge.enable(this);
         setContentView(R.layout.activity_settings);
 
-//        Hooks for Nav viwe
+        // Hooks for Nav view
         drawerLayout = findViewById(R.id.drawer_layout);
         navigationView = findViewById(R.id.nav_view);
         toolbar = findViewById(R.id.toolbar);
-        // drawerLayout = findViewById(R.id.drawer_layout); // This line is redundant, remove it
 
+        // Use this toolbar as the action bar
+        setSupportActionBar(toolbar);
 
-//        Saying use this toolbar as the nav Tool Bar
-        // Use setSupportActionBar for androidx.appcompat.widget.Toolbar
-        setSupportActionBar(toolbar); // <--- CHANGE THIS
+        // --- CHANGE 1: SET THE TITLE ---
+        // Add this line to set the title to "Settings"
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle("Settings");
+        }
 
-
-//        nav drawer menu
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout, toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        // Nav drawer menu toggle
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
 
-
-        // Assuming R.id.drawer_layout is your root view for EdgeToEdge insets,
-        // if R.id.main was a specific content container.
-        // If your DrawerLayout has android:id="@+id/drawer_layout", use that.
-        // If the root is something else, adjust accordingly.
-        // In the previous XML example, the root DrawerLayout had android:id="@+id/drawer_layout"
+        // This listener handles insets (like the status bar) but can interfere with simple layouts.
+        // If your toolbar color doesn't extend to the top of the screen, you might need to adjust this.
+        // For a simple white toolbar, this should be okay.
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.drawer_layout), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            v.setPadding(systemBars.left, 0, systemBars.right, systemBars.bottom); // Set top padding to 0
+            toolbar.setPadding(0, systemBars.top, 0, 0); // Apply top padding to the toolbar instead
             return insets;
         });
     }
+
+    // --- CHANGE 2: INFLATE THE MENU TO SHOW THE SEARCH ICON ---
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.settings_toolbar_menu, menu);
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) searchItem.getActionView();
+        searchView.setQueryHint("Search...");
+        searchView.setMaxWidth(Integer.MAX_VALUE);
+
+        // Optional: Add listeners for search functionality
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // User pressed search button
+                Toast.makeText(SettingsActivity.this, "Searching for: " + query, Toast.LENGTH_SHORT).show();
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                // Text in search field changed
+                return false;
+            }
+        });
+
+        return true;
+    }
+
+    // --- CHANGE 3: HANDLE CLICKS ON MENU ITEMS (LIKE SEARCH) ---
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.action_search) {
+            // This can be used if you want to perform an action when the icon itself is clicked,
+            // but the main logic is often in the SearchView listener above.
+            Toast.makeText(this, "Search icon clicked!", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    // --- Bonus Tip: Handle Navigation Drawer Clicks ---
+    // You'll also want to handle clicks on your navigation items.
+    // Set a listener in onCreate:
+    // navigationView.setNavigationItemSelectedListener(this);
+    // Then implement the listener method.
 }
